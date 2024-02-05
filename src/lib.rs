4fafa8807f1b11719b9cad993e7b33cfb1bc35d6
ForzenString <964413011@qq.com>
2024-02-05 11:39:07 +0800

    ♻️ re-export接口

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,4 @@
 pub mod operations;
 pub mod service;
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-mod entities;
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub use sea_orm;
+pub mod entities;
